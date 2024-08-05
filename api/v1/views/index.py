66c95c1502f6986c05bd58229a8029b53800af32
Marwabadr <elbadrmarwa2002@gmail.com>
2024-08-05 22:42:33 +0000

    index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,22 +1,22 @@
-# Import necessary modules
+#!/usr/bin/python3
+""" Index """
 from api.v1.views import app_views
 from flask import jsonify
-
-# Define a route for the "/status" endpoint
-@app_views.route('/status', methods=['GET'])
-def get_status():
-    return jsonify({"status": "OK"})
-
 from models import storage
 
-# Define a route for the "/stats" endpoint
-@app_views.route('/stats', methods=['GET'])
-def get_stats():
-    # Get a dictionary of class names and their corresponding count
-    stats = {}
-    for cls in storage.classes:
-        count = storage.count(cls)
-        stats[cls] = count
 
-    # Return the dictionary as a JSON response
-    return jsonify(stats)
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    """ Returns JSON """
+    return jsonify(status="OK")
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """ Returns the number of each instance type """
+    return jsonify(amenities=storage.count("Amenity"),
+                   cities=storage.count("City"),
+                   places=storage.count("Place"),
+                   reviews=storage.count("Review"),
+                   states=storage.count("State"),
+                   users=storage.count("User"))
